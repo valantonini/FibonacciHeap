f810e4ae1d93ce44ad1209900ca374204e277e43
val antonini <val.antonini@gmail.com>
2019-09-26 07:52:12 +1000

    Work on successor generation for AStar

--- PerfectPath/PathFinder/AStar.cs
+++ PerfectPath/PathFinder/AStar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using PerfectPath.PriorityQueue;
 
@@ -8,6 +9,7 @@ namespace PerfectPath.PathFinder
         private readonly sbyte[,] _successors = new sbyte[,] { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
         private readonly PathFinderGrid<byte> _grid;
         private readonly AStarCellFComparer _comparer = new AStarCellFComparer();
+        private const int DistanceBetweenCells = 10;
 
         public AStar(PathFinderGrid<byte> grid)
         {
@@ -33,10 +35,42 @@ namespace PerfectPath.PathFinder
                 for (var i = 0; i < _successors.GetLength(0); i++)
                 {
                     var successorX = q.X + _successors[i, 0];
+
+                    // make sure x is in bounds for first and last rows
+                    if (successorX < 0 || successorX >= _grid.Height)
+                    {
+                        continue;
+                    }
+
+                    var successorY = q.Y + _successors[i, 1];
+
+                    //  make sure y is in bounds for first and last columns
+                    if (successorY < 0 || successorY >= _grid.Width)
+                    {
+                        continue;
+                    }
+
+                    var successorCell = new AStarCell()
+                    {
+                        X = successorX,
+                        Y = successorY,
+                        G = q.G + DistanceBetweenCells,
+                        H = CalculateDistanceBetween2Points(end, (successorX, successorY))
+                    };
+                    successorCell.F = successorCell.G + successorCell.H;
                 }
             }
 
             throw new System.NotImplementedException();
+        }
+
+
+        private int CalculateDistanceBetween2Points((int x, int y) position1, (int x, int y) positon2)
+        {
+            var XaMinuxXbSquared = Math.Pow(positon2.x - position1.x, 2);
+            var YaMinusYbSquared = Math.Pow(positon2.y - position1.y, 2);
+
+            return (int)Math.Sqrt(XaMinuxXbSquared + YaMinusYbSquared);
         }
     }
 }
